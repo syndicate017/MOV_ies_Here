efa4c92a80cf5295f28e1736e0076fa58bf0486d
syndicate017 <geniuses17@gmail.com>
2022-03-18 23:42:07 +0700

    Menginisialisasi Project MOV ies Here Application untuk membuat sebuah aplikasi untuk menampilkan data dari Film Box Office menggunakan TMDB API

---
+++ app/src/main/java/com/github/syndicate017/movieshere/Controllers/MainActivity.java
@@ -0,0 +1,15 @@
+package com.github.syndicate017.movieshere.Controllers;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.github.syndicate017.movieshere.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/github/syndicate017/movieshere/Utils/Credentials.java
@@ -0,0 +1,9 @@
+package com.github.syndicate017.movieshere.Utils;
+
+public class Credentials {
+
+    //This app using TMBD API
+    public static final String BASE_URL = "https://api.themoviedb.org";
+    public static final String API_KEY = "166bf0042274ebb1b7ec6dee0da1dee4";
+
+}
